be17d8f6da565da27e34f34a1933e62082a28dda
guijss <guijsilva4@gmail.com>
2024-04-30 12:07:51 -0700

    implemented email service.

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,19 +1,54 @@
 import './contact.scss';
+import { useRef, useState } from 'react';
 import FifteenSlide from '../games/fifteenSlide/FifteenSlide';
 import Mines from '../games/mines/Mines';
+import emailjs from '@emailjs/browser';
 
 const Contact = () => {
+  const formRef = useRef();
+  const [success, setSuccess] = useState(false);
+  const [error, setError] = useState(false);
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm('service_omlbnq7', 'template_py7qpxq', formRef.current, {
+        publicKey: 'Vx03e1LXTNMWbiP83',
+      })
+      .then(
+        () => {
+          setSuccess(true);
+        },
+        (error) => {
+          setError(true);
+          console.log('FAILED...', error.text);
+        }
+      );
+  };
   return (
     <div className="contact">
       <div className="game">
         <FifteenSlide />
       </div>
       <div className="form">
-        <form>
-          <input type="text" required placeholder="Your Name" />
-          <input type="email" required placeholder="Your Email" />
-          <textarea rows={8} placeholder="Message" />
-          <button>Send</button>
+        <form ref={formRef} onSubmit={sendEmail}>
+          <input
+            type="text"
+            required
+            placeholder="Your Name"
+            name="from_name"
+          />
+          <input
+            type="email"
+            required
+            placeholder="Your Email"
+            name="from_email"
+          />
+          <textarea rows={8} placeholder="Message" name="message" />
+          <input className="btn" type="submit" value="Send" />
+          {error && 'Error'}
+          {success && 'Message Sent'}
         </form>
       </div>
       <div className="game">
